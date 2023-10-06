5dd7ff81c3088e719611982a3cbad506bc98e8c2
ElaKedir <eliaskedir516@gmail.com>
2023-10-06 00:47:24 -0700

    My first commit on C preprocessing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,3 @@
 #include <stdio.h>
 #include <stdint.h>
-
-/**
-* main - Entry point
-*
-* Return: 0 (succes)
-*/
-
-int main(void)
-{
-printf("Size of a char: %zu bytes(s)\n", sizeof(char));
-printf("Size of an int: %zu bytes(s)\n", sizeof(int));
-printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %zu bytes(s)\n", sizeof(float));
-return (0);
-}
+int main(void){printf("Size of a char: %zu byte(s)\n",sizeof(char));printf("Size of an int: %zu byte(s)\n",sizeof(int));printf("Size of a long int: %zu byte(s)\n",sizeof(long int));printf("Size of a long long int: %zu byte(s)\n",sizeof(long long int));printf("Size of a float: %zu byte(s)\n",sizeof(float));return 0;}
