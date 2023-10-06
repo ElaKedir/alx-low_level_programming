77fc39a2e2a078fcaf8d0bf6acfbcba849aa0b8f
ElaKedir <eliaskedir516@gmail.com>
2023-10-06 10:59:30 -0700

    My first commit on C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,10 @@
  * Return: 1 (Success)
  */
 
-int main(void) {
-  char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(STDERR_FILENO, str, strlen(str));
-  return 1;
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(STDERR_FILENO, str, strlen(str));
+return (1);
 }
 
