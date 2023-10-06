ff21b7fd6bf7a37e2ea0bcdc18206c06f295943f
ElaKedir <eliaskedir516@gmail.com>
2023-10-06 10:42:09 -0700

    My first commit on C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void) {
+  char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+  write(STDERR_FILENO, str, strlen(str));
+  return 1;
+}
+
