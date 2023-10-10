df563bdc3565faa696ae7305e6b7b2ad76ea135c
ElaKedir <eliaskedir516@gmail.com>
2023-10-10 16:05:05 +0300

    c programming project on combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,22 +5,23 @@ int main()
     int i = 0;
     int j = 0;
     int combinationnumber = 0;
-    int totalcombinations = 46;
-    for(i = 0; i <=9; i++)
+    int totalcombinations = 45;
+
+    for(i = 0; i <= 9; i++)
     {
-        for(j = 0; j<=9; j++)
+        for(j = 0; j <= 9; j++)
         {
             if(i < j)
             {
                 putchar(i + '0');
                 putchar(j + '0');
+
                 combinationnumber++;
-                if(combinationnumber < totalcombinations)
+                if (combinationnumber < totalcombinations)
                 {
-                putchar(',');
-                putchar(' ');
+                    putchar(',');
+                    putchar(' ');
                 }
-                
             }
         }
     }
