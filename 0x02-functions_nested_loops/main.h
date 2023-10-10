703a3805206464ff4452a0b43f0aced34862a710
ElaKedir <eliaskedir516@gmail.com>
2023-10-10 08:25:55 -0700

    Adding main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,17 @@
 #ifndef main.h
 #define main.h
 
-int main (void)
+int main(void)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
-
 #endif
