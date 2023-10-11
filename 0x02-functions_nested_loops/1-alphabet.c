bd53531891773168115a0aea449c9ffa87d734a1
ElaKedir <eliaskedir516@gmail.com>
2023-10-11 11:22:54 +0300

    Adding main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
-* main - Entry point
 * print_alphabet - Prints the lowercase alphabet followed by a new line.
-* Return: Alwyas 0 (success)
 */
 
 void print_alphabet(void)
