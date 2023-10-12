61e43e16cf40dd7355e4cca6fdcda447c12d8742
ElaKedir <eliaskedir516@gmail.com>
2023-10-12 18:10:26 +0300

    second round project on working with functions and prototypes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* more_numbers - that prints numbers from 0 to 9 except 2 and 4.
+* Return: 0 (Success)
+*/
+
+void more_numbers(void)
+{
+int i, j;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+putchar((i / 10) + '0');
+}
+putchar((i % 10) + '0');
+}
+putchar('\n');
+}
+
+putchar('\n');
+}
