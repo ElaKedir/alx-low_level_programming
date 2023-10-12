c2ce884fd3de8c8e1b0afd295d5e4785af8dd2fe
ElaKedir <eliaskedir516@gmail.com>
2023-10-12 16:16:10 +0300

    second round project on working with functions and prototypes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if the character is uppercase and 0 if it is not.
+ * @c: the character
+ * Return: 0 (Success)
+ */
+
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
