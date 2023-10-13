b95017033f9112c695f581b5a08e735125175945
ElaKedir <eliaskedir516@gmail.com>
2023-10-12 17:13:56 -0700

    Adding main.h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+
+{
+int i;
+
+int fizz;
+int buzz;
+
+for(i = 1; i <= 100;i++)
+{
+if((i % 3) == 0)
+{
+printf ("fizz ");
+
+}
+else if ((i % 5) == 0 && i != 100)
+{
+printf("buzz ");
+}
+else if ((i % 5) == 0 && i == 100)
+{
+printf("buzz");
+}
+else
+
+{
+if (i < 10)
+{
+putchar(i + '0');
+putchar(' ');
+
+}
+else if (i < 100)
+{
+putchar((i / 10) % 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+}
+else
+{
+putchar((i / 100) % 10 + '0');
+putchar((i / 10) % 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+}
+
