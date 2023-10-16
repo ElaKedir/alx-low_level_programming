5dc378c08f56c2ddb4c3c991a2774463a574ceb0
ElaKedir <eliaskedir516@gmail.com>
2023-10-16 15:58:03 +0300

    poiinters and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*reset_t0_98 - a function to reset the int to 98.
-*@n: the variable n
-*Return - 0 (success)
+* reset_to_98 - a function to reset the int to 98.
+* @n: the variable n
+* Return - 0 (success)
 */
- 
- void reset_to_98(int *n)
+
+void reset_to_98(int *n)
 
 {
-    int *n = 98;
+*n = 98;
 }
