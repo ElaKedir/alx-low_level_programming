b381c97b006c21a577076b9d15bddf6129f433f8
ElaKedir <eliaskedir516@gmail.com>
2023-10-16 13:25:45 +0300

    arrays

---
+++ 0x03-debugging/101-print_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_number(int n)
+{
+if (n == 0)
+{
+putchar('0');
+return;
+}
+
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+
+int divisor = 1;
+while (n / divisor > 9)
+{
+divisor *= 10;
+}
+
+while (divisor > 0)
+{
+putchar((n / divisor) + '0');
+n %= divisor;
+divisor /= 10;
+}
+}
