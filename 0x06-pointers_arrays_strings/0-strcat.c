7ba87b99264448bcd478434f12c83c91541ac91d
ElaKedir <eliaskedir516@gmail.com>
2023-10-19 10:14:12 +0300

    more pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+*_strcat - a function to append a string into another.
+*@dest: The string that is going to be appended to
+*@src: The string that will be appended
+*Return: 0 (Success)
+*/
+
 char *_strcat(char *dest, char *src)
 {
-    strcat(dest, src);
+strcat(dest, src);
 }
