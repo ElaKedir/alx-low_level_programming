6e63008cd9c583b409eb1600e1fddbf523b54386
ElaKedir <eliaskedir516@gmail.com>
2023-10-23 16:16:04 +0300

    even more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - the function to mention or find the first character in a string
+ * @c: the character.
+ * @s: the string.
+ * Return: A pointer to the changed string.
+ */
+
+char *_strchr(char *s, char c)
+{
+return strchr(s, c);
+}
