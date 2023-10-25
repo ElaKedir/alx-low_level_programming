04af6871391dd8d74b98258f3874ab629dcc2932
ElaKedir <eliaskedir516@gmail.com>
2023-10-25 11:50:14 +0300

    recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - _puts_recursion - the function
+ * @s: the string
+ * Return: Always 0.
+ */
+
+ void _puts_recursion(char *s)
+ {
+     if (*s != '\0')
+     {
+         putchar(*s);
+         _puts_recursion(s + 1);
+     }
+     else
+     {
+     printf("\n");
+     }
+ }
