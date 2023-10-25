31b5cf329bff892fea170e94cceffab06a39fb0b
ElaKedir <eliaskedir516@gmail.com>
2023-10-25 10:57:51 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - the function
+ * @s: the string
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+	{
+	return (0);
+	}
+	else
+	{
+	return (1 + _strlen_recursion(s + 1));
+	}
+}
+
