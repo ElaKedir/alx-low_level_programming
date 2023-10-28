be173eb255fb2bafce820d97d97341063c4ac464
ElaKedir <eliaskedir516@gmail.com>
2023-10-27 17:17:14 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,13 +25,13 @@ int check_num(char *str)
 		return (1);
 	}
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Entry point of the program
+ * @argc: Count of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 	int main(int argc, char *argv[])
 	{
