c71a870d781a29104464e578dbae9282b8768055
ElaKedir <eliaskedir516@gmail.com>
2023-11-02 11:30:39 -0700

    more malloc...

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = l2;
 	}
 
-	ptr = malloc(sizeof(char) * (l1 + l2 + 1));
+	ptr = malloc(sizeof(char) * (l1 + n + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
