85fd52120cff6785b64bba80267a36198fc3fadf
ElaKedir <eliaskedir516@gmail.com>
2024-02-03 02:01:57 +0300

    initial commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creates a hash table
+ with a given size
+ *
+ * @size: size of the hash table
+ * Return: the created hash table, or NULL if the function fails
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i = 0;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+	    array[i] = NULL;
+	    i++;
+	}
+
+	table->array = array;
+	table->size = size;
+
+	return (table);
+}
